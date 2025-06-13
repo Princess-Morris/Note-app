42dc9cc54a3cd1779a74d10f48831de6414fce3e
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-13 19:36:10 +0100

    Update add stack screen options for note page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -18,6 +18,8 @@ const RootLayout = () => {
     }
   }}>
     <Stack.Screen name="index" options={{title: "Home"}} />
+   <Stack.Screen name="notes" options={{headerTitle: "Notes"}} />
+
   </Stack>
 }
 
