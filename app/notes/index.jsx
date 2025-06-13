d896cf721ea34200fe133ad47e8746767dd8eb7a
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-13 19:39:27 +0100

    Update: Add notes array

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const NoteScreen = () => {
+    const [notes, setNotes] = useState([
+        {id: "1", text: "Note One"}
+        {id: "1", text: "Note Two"},
+        {id: "1", text: "Note Three"},
+    ])
     return (
         <View style={styles.container}>
             <Text>Notes</Text>
