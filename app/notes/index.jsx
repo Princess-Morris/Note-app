76d0d96ceeeb693390d3722172b87274b2d2baea
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-14 04:01:52 +0100

    Style styling modal

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -58,10 +58,9 @@ const NoteScreen = () => {
                         <Text style={styles.cancelButtonText}>Cancel</Text>
 
                        </TouchableOpacity>
-                       <TouchableOpacity style={styles.addButton} 
-                       onPress = {() => setModalVisible(false)}
+                       <TouchableOpacity style={styles.saveButton} 
                        >
-                        <Text style={styles.cancelButtonText}>Cancel</Text>
+                        <Text style={styles.saveButtonText}>Save</Text>
 
                        </TouchableOpacity>
                    </View>
@@ -106,16 +105,57 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     modalOverlay:{
-
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     modalContent: {
-        
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 10,
+        width: '#80%',
     },
     modalTitle: {
-
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
     },
     input:{
-
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 8,
+        padding: 10,
+        fontSize: 16,
+        marginBottom: 15
+    },
+    modalButtons:{
+       flexDirection: 'row',
+       justifyContent: 'space-between'
+    },
+    cancelButton: {
+       backgroundColor: '#ccc',
+       padding: 10,
+       borderRadius: 5,
+       flex: 1,
+       marginRight: 10,
+       alignItems: 'center',
+    },
+    cancelButtonText:{
+      fontSize: 16,
+      color: '#333',
+    },
+    saveButton:{
+       backgroundColor: '#007bff',
+       padding: 10,
+       borderRadius: 5,
+       flex: 1,
+       alignItems: 'center'
+    },
+    saveButtonText:{
+        fontSize: 16,
+        color: '#fff',
     }
 })
 
